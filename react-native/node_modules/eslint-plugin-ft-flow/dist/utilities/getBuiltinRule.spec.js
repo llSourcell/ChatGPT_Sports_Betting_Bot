bdcc51cc44f39a70b89b00e62e29bae128bf8ab9
dinamitrii <k_karaivanov@abv.bg>
2024-01-16 02:11:22 +0200

    some fixes again

--- react-native/node_modules/eslint-plugin-ft-flow/dist/utilities/getBuiltinRule.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _getBuiltinRule = require("./getBuiltinRule");
-
-describe('getBuiltinRule', function () {
-  it('works with jest', function () {
-    (0, _getBuiltinRule.getBuiltinRule)('no-undef');
-  });
-});
--- react-native/node_modules/eslint-plugin-ft-flow/dist/utilities/suppressionTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = ['$FlowFixMe', '$FlowExpectedError', '$FlowIssue', '$FlowIgnore'];
-exports["default"] = _default;
-module.exports = exports.default;
